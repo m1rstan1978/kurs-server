00227142737809c5f697506e024dcfd0127ebab2
m1rstan1978 <m1rstan@mail.ru>
2023-11-13 00:28:33 +0300

    fix

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ const fileUpload = require('express-fileupload');
 const PORT = process.env.PORT || 8000
 
 const app = express()
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://kurs-psi.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
